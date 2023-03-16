ad6094ad5376ff98f0b4ac677cc4033e43265054
Jason T. Greene <jason.greene@redhat.com>
2023-03-16 18:29:30 -0500

    Fixup stop operation
    Signed-off-by: Jason T. Greene <jason.greene@redhat.com>

--- pkg/hypervctl/error.go
+++ pkg/hypervctl/error.go
@@ -3,7 +3,10 @@
 
 package hypervctl
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // VM State errors
 var (
@@ -44,4 +47,71 @@ func (e DestroySystemResult) Reason() string {
 		return "invalid state"
 	}
 	return "Unknown"
+}
+
+// Shutdown operation error codes
+const (
+	ErrShutdownFailed           = 32768
+	ErrShutdownAccessDenied     = 32769
+	ErrShutdownNotSupported     = 32770
+	ErrShutdownStatusUnkown     = 32771
+	ErrShutdownTimeout          = 32772
+	ErrShutdownInvalidParameter = 32773
+	ErrShutdownSystemInUse      = 32774
+	ErrShutdownInvalidState     = 32775
+	ErrShutdownIncorrectData    = 32776
+	ErrShutdownNotAvailable     = 32777
+	ErrShutdownOutOfMemory      = 32778
+	ErrShutdownFileNotFound     = 32779
+	ErrShutdownNotReady         = 32780
+	ErrShutdownMachineLocked    = 32781
+	ErrShutdownInProgress       = 32782
+)
+
+
+type shutdownCompError struct {
+	errorCode int
+	message   string
+}
+
+func (s *shutdownCompError) Error() string {
+	return fmt.Sprintf("%s (%d)", s.message, s.errorCode)
+}
+
+func translateShutdownError(code int) error {
+	var message string
+	switch code {
+	case ErrShutdownFailed:
+		message = "shutdown failed"
+	case ErrShutdownAccessDenied:
+		message = "access was denied"
+	case ErrShutdownNotSupported:
+		message = "shutdown not supported by virtual machine"
+	case ErrShutdownStatusUnkown:
+		message = "virtual machine status is unknown"
+	case ErrShutdownTimeout:
+		message = "timeout starting shutdown"
+	case ErrShutdownInvalidParameter:
+		message = "invalid parameter"
+	case ErrShutdownSystemInUse:
+		message = "system in use"
+	case ErrShutdownInvalidState:
+		message = "virtual machine is in an invalid state for shutdown"
+	case ErrShutdownIncorrectData:
+		message = "incorrect data type"
+	case ErrShutdownNotAvailable:
+		message = "system is not available"
+	case ErrShutdownOutOfMemory:
+		message = "out of memory"
+	case ErrShutdownFileNotFound:
+		message = "file not found"
+	case ErrShutdownMachineLocked:
+		message = "machine is locked and cannot be shut down without the force option"
+	case ErrShutdownInProgress:
+		message = "shutdown is already in progress"
+	default:
+		message = "unknown error"
+	}
+
+	return &shutdownCompError{code, message}
 }
