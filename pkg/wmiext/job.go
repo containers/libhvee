567e418ce77110bf83e56f1db57d25987ea3e74d
lstocchi <lstocchi@redhat.com>
2026-01-20 18:15:42 +0100

    add user-friendly error descriptions to JobError
    This commit extracts ErrorDescription from the updated Hyperv job object fetched inside WaitJob.
    
    What happens is that Hyper-V starts the job in background and immediately returns a
    reference to it which is saved by the BeginInvoke().Out() to an object.
    If we do not refresh this object, it's like we are always working on an old
    snapshot and the description will never be updated.
    
    Inside WaitJob the object is re-fetched and it contains the updated
    error message. By reusing it, we can provide a detailed error
    description instead of just error code.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- pkg/wmiext/job.go
+++ pkg/wmiext/job.go
@@ -4,14 +4,19 @@ package wmiext
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 type JobError struct {
-	ErrorCode int
+	ErrorCode   int
+	Description string
 }
 
 func (err *JobError) Error() string {
+	if err.Description != "" {
+		return fmt.Sprintf("Job failed with error code: %d. Description: %s", err.ErrorCode, err.Description)
+	}
 	return fmt.Sprintf("Job failed with error code: %d", err.ErrorCode)
 }
 
@@ -45,7 +50,17 @@ func WaitJob(service *Service, job *Instance) error {
 	}
 
 	if result.(int32) != 0 {
-		return &JobError{ErrorCode: int(result.(int32))}
+		desc := ""
+
+		if desc, err = job.GetAsString("ErrorDescription"); err == nil {
+			desc = strings.ReplaceAll(desc, "\n", " ")
+			desc = strings.TrimSpace(desc)
+		}
+
+		return &JobError{
+			ErrorCode:   int(result.(int32)),
+			Description: desc,
+		}
 	}
 
 	return nil
